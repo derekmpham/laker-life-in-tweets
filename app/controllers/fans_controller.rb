0fba43a50d4cd40c92a7cc5b3cd7d8f663447cfb
Derek Pham <derekmpham@gmail.com>
2016-12-30 18:16:57 -0800

    Allow app to pull more fan tweets

--- app/controllers/fans_controller.rb
+++ app/controllers/fans_controller.rb
@@ -1,8 +1,8 @@
 # Show fans' tweets
 get '/fans' do
   # Create variable for each hashtag
-  @lakers = $client.search("#lakers -rt").first.text
-  @lakernation = $client.search("#lakernation -rt").first.text
-  @lakeshow = $client.search("#lakeshow -rt").first.text
+  @lakers = $client.search("#lakers -rt").take(6).collect
+  @lakernation = $client.search("#lakernation -rt").take(6).collect
+  @lakeshow = $client.search("#lakeshow -rt").take(6).collect
   erb :'fans/index'
 end
